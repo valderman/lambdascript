3ccae4f4a40b92406680c03d71f02cc2db89abc9
Anton Ekblad <anton@ekblad.cc>
2010-08-07 01:03:23 +0200

    Add support for looking up type structures to the type checker monad.

--- src/LambdaScript/TCM.hs
+++ src/LambdaScript/TCM.hs
@@ -5,7 +5,8 @@ module LambdaScript.TCM (
     TCM,
     runIn, runIn', run, blankEnv,
     pushScope, popScope,
-    typeOf, declare, declaredInThisScope
+    typeOf, declare, declaredInThisScope,
+    getType, newType
   ) where
 import Data.Map as M
 import Control.Monad.State as CMS
@@ -16,8 +17,11 @@ import LambdaScript.ErrM
 --   This is needed because we want to be able to shadow symbols declared in
 --   outer scopes while still being able to error out if the user tries to
 --   declare the same symbol twice in the same scope.
-newtype TCState = TCState {
-    env :: [Map String Type]
+--   The type checker state also includes a mapping from type names to their
+--   actual structure, so we know what (TIdent "Tree") means in concrete terms.
+data TCState = TCState {
+    env   :: [Map String Type],
+    types :: Map String Type
   } deriving Show
 
 newtype TCM a = TCM {
@@ -43,21 +47,24 @@ run a =
 -- | Blank environment.
 blankEnv :: TCState
 blankEnv =
-  TCState [empty]
+  TCState {
+    env   = [empty],
+    types = empty
+  }
 
 -- | Push a new scope onto the scope stack. New symbols are always declared
 --   in the top scope on the stack.
 pushScope :: TCM ()
 pushScope =
-  get >>= \(TCState envs) -> put $ TCState $ empty:envs
+  get >>= \st -> put st {env = empty:env st}
 
 -- | Remove the topmost scope from the scope stack.
 popScope :: TCM ()
 popScope =
-  get >>= \(TCState envs) ->
-      case envs of
+  get >>= \st ->
+      case env st of
         [_] -> fail "The top level scope can't be popped!"
-        _   -> put $ TCState $ envs
+        _   -> put st {env = tail $ env st}
 
 -- | Return the type of a symbol, failing if the symbol hasn't been declared.
 --   If a symbol has been shadowed, the shadowing symbol is always returned,
@@ -65,8 +72,8 @@ popScope =
 --   scope.
 typeOf :: String -> TCM Type
 typeOf s = do
-  TCState envs <- get
-  case lookup' s envs of
+  st <- get
+  case lookup' s (env st) of
     Just t -> return t
     _      -> fail $ "Symbol '" ++ s ++ "' hasn't been declared!"
   where
@@ -82,15 +89,34 @@ typeOf s = do
 --   lifetime of the current scope.
 declare :: String -> Type -> TCM ()
 declare s t = do
-  TCState (env:envs) <- get
-  put $ TCState $ (insert s t env) : envs
+  st <- get
+  let (e:envs) = env st
+  put $ st {env = (insert s t e) : envs}
 
 -- | Check whether the given symbol was declared in the current scope or not.
 --   A symbol with an unknown type is treated as though it hasn't been
 --   declared.
 declaredInThisScope :: String -> TCM Bool
 declaredInThisScope id = do
-  TCState (env:_) <- get
-  case M.lookup id env of
+  st <- get
+  case M.lookup id (head $ env st) of
     Just x | x /= TUnknown -> return True
     _                      -> return False
+
+-- | Get the concrete representation of the given type. Asking for a
+--   nonexistent type gives an error.
+getType :: String -> TCM Type
+getType id = do
+  st <- get
+  case M.lookup id (types st) of
+    Just t -> return t
+    _      -> fail $ "Type '" ++ id ++ "' hasn't been declared!"
+
+-- | Declare a new type. Types don't change at all during type checking, so
+--   here trying to redeclare a type produces an error.
+newType :: String -> Type -> TCM ()
+newType id t = do
+  st <- get
+  case M.lookup id (types st) of
+    Nothing -> put st {types = insert id t (types st)}
+    _       -> fail $ "Type '" ++ id ++ "' already declared!"
