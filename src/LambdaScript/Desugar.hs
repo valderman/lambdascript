3379c9f3f42f2b35ce060101f08525a0f13a4253
Anton Ekblad <anton@ekblad.cc>
2010-09-01 00:27:51 +0200

    Desugar definitions inside expressions too.

--- src/LambdaScript/Desugar.hs
+++ src/LambdaScript/Desugar.hs
@@ -10,8 +10,14 @@ generatedVarPrefix = '*'
 -- | Perform desugaring on the given program.
 desugar :: Program -> Program
 desugar (Program defs) =
-  Program $ map deFunDefs $ groupDefs (filter isFunDef defs)
+  Program $ desuDefs defs
+
+-- | Desugar a list of definitions.
+desuDefs :: [Def] -> [Def]
+desuDefs defs =
+  other ++ (map deFunDefs $ groupDefs funs)
   where
+    (funs, other) = partition isFunDef defs
     isFunDef (FunDef _ _) = True
     isFunDef _            = False
 
@@ -79,6 +85,55 @@ casefy (TPGuards pats guards) =
     top2case = map (\(GuardedTopExpr guard ex) -> GuardedCaseExpr guard ex)
 casefy (TPNoGuards pats ex) =
   ECase (pats2Tuple pats) [CPNoGuards (PTuple $ map PatC pats) ex]  
+
+
+-- | Calculate the first hand dependencies of the given expression.
+--   First hand, as in only the explicit, non-recursive dependencies of this
+--   expression.
+desuExpr :: Expr -> Expr
+desuExpr = go
+  where
+    go (EList exs)       = EList (map desuExpr exs)
+    go (ETuple exs)      = ETuple (map desuExpr exs)
+    go (EApp f x)        = EApp (desuExpr f) (desuExpr x) 
+    go (ECons x xs)      = ECons (desuExpr x) (desuExpr xs)
+    go (EConcat xs ys)   = EConcat (desuExpr xs) (desuExpr ys)
+    go (EMul a b)        = EMul (desuExpr a) (desuExpr b)
+    go (EDiv a b)        = EDiv (desuExpr a) (desuExpr b)
+    go (EAdd a b)        = EAdd (desuExpr a) (desuExpr b)
+    go (ESub a b)        = ESub (desuExpr a) (desuExpr b)
+    go (EMod a b)        = EMod (desuExpr a) (desuExpr b)
+    go (EEq a b)         = EEq (desuExpr a) (desuExpr b)
+    go (ELT a b)         = ELT (desuExpr a) (desuExpr b)
+    go (EGT a b)         = EGT (desuExpr a) (desuExpr b)
+    go (ELE a b)         = ELE (desuExpr a) (desuExpr b)
+    go (EGE a b)         = EGE (desuExpr a) (desuExpr b)
+    go (ENE a b)         = ENE (desuExpr a) (desuExpr b)
+    go (ELambda v x)     = ELambda v (desuExpr x)
+    go (EIf a b c)       = EIf (desuExpr a) (desuExpr b) (desuExpr c)
+    go (ECase ex pats)   = ECase (desuExpr ex) (map desuCasePat pats)
+    go (EBinds ex defs)  = EBinds (desuExpr ex) (desuDefs defs)
+    go expr              = expr
+
+-- | Desugar a case pattern.
+desuCasePat :: CasePattern -> CasePattern
+desuCasePat (CPGuards pat guardeds) =
+  CPGuards (desuPat pat) (map desuGuarded guardeds)
+desuCasePat (CPNoGuards pat expr) =
+  CPNoGuards (desuPat pat) (desuExpr expr)
+
+-- | Desugar a guarded case expression.
+desuGuarded :: GuardedCaseExpr -> GuardedCaseExpr
+desuGuarded (GuardedCaseExpr (GuardExpr guard) expr) =
+  GuardedCaseExpr (GuardExpr $ desuExpr guard) (desuExpr expr)
+
+-- | Desugar a pattern.
+desuPat :: Pattern -> Pattern
+desuPat (PList pats)      = PList $ map (\(PatC p) -> PatC $ desuPat p) pats
+desuPat (PTuple pats)     = PTuple $ map (\(PatC p) -> PatC $ desuPat p) pats
+desuPat (PConstr id pats) = PConstr id $ map desuPat pats
+desuPat (PCons p1 p2)     = PCons (desuPat p1) (desuPat p2)
+desuPat pat               = pat
 
 -- | Creates a tuple expression for pattern matching on function arguments in
 --   the function body redurned by lambdafy.
