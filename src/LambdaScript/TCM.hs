783627f89289cb6d2c86ed0490c0701a2746fde6
Anton Ekblad <anton@ekblad.cc>
2010-09-08 20:14:57 +0200

    Add a monad for type checking.
    As with the previous commit, see the paper "Typing Haskell in Haskell" for
    more information.

---
+++ src/LambdaScript/TCM.hs
@@ -0,0 +1,35 @@
+module LambdaScript.TCM where
+import LambdaScript.Types
+
+newtype TCM a = TCM {runT :: Int -> Subst -> (a, Int, Subst)}
+
+type Infer e t = [Assump] -> e -> TCM t
+
+instance Monad TCM where
+  return x = TCM $ \n s -> (x, n, s)
+  m >>= f  = TCM $ \n s -> case runT m n s of
+                             (a, n', s') -> runT (f a) n' s'
+
+-- | Create a new type variable with the given kind.
+newTVar :: Kind -> TCM AbsType
+newTVar k = TCM $ \n s -> (ATVar $ ATyvar (enumId n) k, n+1, s)
+
+-- | Return the current substitution
+getSubst :: TCM Subst
+getSubst = TCM $ \n s -> (s, n, s)
+
+-- | Extend the current substitution
+extSubst :: Subst -> TCM ()
+extSubst s' = TCM $ \n s -> ((), n, s' `compose` s)
+
+-- | Instantiate a type scheme; this entails binding a new type variable to
+--   each generic, quantified variable of the type.
+instantiate :: Scheme -> TCM AbsType
+instantiate (Forall ks t) =
+  mapM newTVar ks >>= \ts -> return $ inst ts t
+
+-- | Run a type checking computation.
+runTCM :: TCM a -> a
+runTCM (TCM f) =
+  case f 0 nullSubst of
+    (x, n, s) -> x
