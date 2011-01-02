f7bd0964c4091ed80b1948b206109b405f16dcde
Anton Ekblad <anton@ekblad.cc>
2011-01-02 14:26:13 +0100

    lsc foo.ls now writes its output to foo.js if compilation is successful.

--- src/Main.hs
+++ src/Main.hs
@@ -10,15 +10,21 @@ import LambdaScript.Print
 import LambdaScript.ErrM
 import LambdaScript.TypeChecker
 import LambdaScript.CodeGen.Generate
+import Control.DeepSeq
 
 main :: IO ()
 main = do
   (fp:_) <- getArgs
-  putStrLn $ "Checking " ++ fp
   p <- parseAndCheck fp
-  putStrLn $ printTree p
-  putStrLn "\n\n\n\n"
-  putStrLn $ show $ generate p
+  -- Really ugly way of ensuring p' is evaluated enough to spot any errors
+  -- before we start writing any code to file without having to write NFData
+  -- instances for the intermediate code types.
+  case generate p of
+    p' | length p' /= -1 -> do
+      let jsfile = reverse $ 's':'j':dropWhile (/= '.') (reverse fp)
+      runtime <- readFile "lib/runtime.js"
+      let rt = unlines [x | x <- lines runtime, not (null x) && take 2 x /= "//"]
+      writeFile jsfile $ rt ++ show p'
 
 -- | Parse and type check a program.
 parseAndCheck :: FilePath -> IO Program
