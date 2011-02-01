4f33224c31efbc2ad4d0493371d511daabba410b
Anton Ekblad <anton@ekblad.cc>
2011-02-01 14:35:28 +0100

    Compare to undefined instead using typeof to check if a thunk is evaluated.
    Previously we called a function _u(o) that did typeof o.x == 'undefined' to
    check if the thunk o is previously evaluated or not. Not only does that
    require a staggering amount of function calls, but also includes expensive
    string comparisons unless the JS interpreter does a good job optimizing that
    particular idiom. Comparing an attribute to undefined should be much faster,
    and brings the total number of function calls for the Tetris example down
    to "only" 1/3 million per second (from the old ~1.1M.)

--- src/LambdaScript/CodeGen/ShowJS.hs
+++ src/LambdaScript/CodeGen/ShowJS.hs
@@ -6,7 +6,7 @@ import Data.List (intercalate)
 
 instance Show Exp where
   show (Thunk ex) =
-    "function _(){if(_u(_)) _.x=" ++ show ex ++ ";return _.x;}"
+    "function _(){if(_.x===$u) _.x=" ++ show ex ++ ";return _.x;}"
   show (IOThunk ex) =
     "function _(){return " ++ show ex ++ ";}"
   show (Eval ex) =
@@ -44,7 +44,7 @@ instance Show Exp where
 
 instance Show Stmt where
   show (SelfThunk id ss) =
-    "if(_u($._" ++ id ++ ")){" ++ concat (map show ss) ++ ";}return $._" ++ id ++ ".x;"
+    "if($u===$._" ++ id ++ ".x){" ++ concat (map show ss) ++ ";}return $._" ++ id ++ ".x;"
   show (Assign v@(Global _ id) ex) = show v ++ " = " ++ show ex ++ ";\n"
   show (Assign v ex) = "var " ++ show v ++ " = " ++ show ex ++ ";\n"
   show (If ex th el) = "if(" ++ show ex ++ ") " ++
